40d8e8976340a693730425a132217fb6657e3f47
vincenzo <vincenzo.scialpi-sullivan@lothrin.com>
2024-02-19 22:15:10 +0000

    Set up CalibrationDocumentProcessorTests to be able to switch to live input from test input

--- AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
+++ AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
@@ -5,12 +5,15 @@ namespace AdventOfCode2023.Tests.Day01;
 [TestFixture]
 public class CalibrationDocumentProcessorTests
 {
+    private const string TestInput = "Day01/Resources/TestInput.txt";
+    private const string LiveInput = "Day01/Resources/Input.txt";
+    
     private CalibrationDocumentProcessor _calibrationDocumentProcessor = null!;
     
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        _calibrationDocumentProcessor = new CalibrationDocumentProcessor("Day01/Resources/TestInput.txt");
+        _calibrationDocumentProcessor = new CalibrationDocumentProcessor(TestInput);
     }
     
     [Test]
