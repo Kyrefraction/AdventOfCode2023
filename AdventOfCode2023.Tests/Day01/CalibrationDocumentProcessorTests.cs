d4c1756dcb2b318902fc4ca2dfd9ea6d8ebc68cf
AshJPeek <a.j.peek97@gmail.com>
2024-02-21 21:31:32 +0000

    swapped test input for live input

--- AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
+++ AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
@@ -6,20 +6,22 @@ namespace AdventOfCode2023.Tests.Day01;
 public class CalibrationDocumentProcessorTests
 {
     private const string TestInput = "Day01/Resources/TestInput.txt";
-    
-    
+
+    private const string LiveInput = "Day01/Resources/Input.txt";
+
+
     private CalibrationDocumentProcessor _calibrationDocumentProcessor = null!;
     
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        _calibrationDocumentProcessor = new CalibrationDocumentProcessor(TestInput);
+        _calibrationDocumentProcessor = new CalibrationDocumentProcessor(LiveInput);
     }
     
     [Test]
     public void CalculateTotalCalibrationValue()
     {
         var result = _calibrationDocumentProcessor.CalculateTotalCalibrationValue();
-        Assert.That(result, Is.EqualTo(142));
+        Assert.That(result, Is.EqualTo(55172));
     }
 }
