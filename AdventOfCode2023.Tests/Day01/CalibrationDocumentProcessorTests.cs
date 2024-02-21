d308c5b95c4dbec25eda5777157cfbafede56c1f
vincenzo <vincenzo.scialpi-sullivan@lothrin.com>
2024-02-21 22:49:59 +0000

    Parameterised CalculateTotalCalibrationValue test, allowing for test of the test input AND the live input

--- AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
+++ AdventOfCode2023.Tests/Day01/CalibrationDocumentProcessorTests.cs
@@ -9,20 +9,15 @@ public class CalibrationDocumentProcessorTests
 {
     private const string TestInput = "Day01/Resources/TestInput.txt";
     private const string LiveInput = "Day01/Resources/Input.txt";
+    private const string TestInputAnswer = "Day01/Resources/TestInputAnswer.txt";
+    private const string LiveInputPartOneAnswer = "Day01/Resources/PartOneAnswer.txt";
     
-    private CalibrationDocumentProcessor _calibrationDocumentProcessor = null!;
-    private readonly int _partOneAnswer = FileReader.ExtractInput("Day01/Resources/PartOneAnswer.txt").ToInt();
-    
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
+    [TestCase(TestInput, TestInputAnswer)]
+    [TestCase(LiveInput, LiveInputPartOneAnswer)]
+    public void CalculateTotalCalibrationValue(string resourcePath, string answerPath)
     {
-        _calibrationDocumentProcessor = new CalibrationDocumentProcessor(LiveInput);
-    }
-    
-    [Test]
-    public void CalculateTotalCalibrationValue()
-    {
-        var result = _calibrationDocumentProcessor.CalculateTotalCalibrationValue();
-        Assert.That(result, Is.EqualTo(_partOneAnswer));
+        var calibrationDocumentProcessor = new CalibrationDocumentProcessor(resourcePath);
+        var result = calibrationDocumentProcessor.CalculateTotalCalibrationValue();
+        Assert.That(result, Is.EqualTo(FileReader.ExtractInput(answerPath).ToInt()));
     }
 }
